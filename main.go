e2353759e6371b54f1e3efe97d05df5544c98574
Memory Clutter <memclutter@gmail.com>
2018-06-03 22:14:33 +0300

    Simple web server template

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/buaazp/fasthttprouter"
+	"github.com/valyala/fasthttp"
+)
+
+// Default CLI argument values
+const (
+	DefaultAddr = ":8000"
+)
+
+// CLI arguments
+var (
+	addr string
+)
+
+// Init func
+func init() {
+	flag.StringVar(&addr, "addr", DefaultAddr, "Server host and port")
+}
+
+// Main func
+func main() {
+	// Parse CLI arguments
+	flag.Parse()
+
+	// Listen and serve
+	log.Print(fasthttp.ListenAndServe(addr, ApplyMiddleware(NewRouter().Handler)))
+}
+
+// Create new http router
+func NewRouter() *fasthttprouter.Router {
+	router := fasthttprouter.New()
+
+	// TODO: routes here
+
+	return router
+}
+
+// Apply all middleware
+func ApplyMiddleware(h fasthttp.RequestHandler) fasthttp.RequestHandler {
+	// TODO: apply middleware here
+
+	return h
+}
