e4b8d0238a9860e7764806f29a190e6071267639
Memory Clutter <memclutter@gmail.com>
2018-06-03 23:06:12 +0300

    Mongodb connection

--- main.go
+++ main.go
@@ -5,28 +5,45 @@ import (
 	"log"
 
 	"github.com/buaazp/fasthttprouter"
+	"github.com/globalsign/mgo"
 	"github.com/valyala/fasthttp"
 )
 
 // Default CLI argument values
 const (
-	DefaultAddr = ":8000"
+	DefaultAddr       = ":8000"
+	DefaultMongodbUrl = "mongodb://localhost:27017/user"
 )
 
 // CLI arguments
 var (
-	addr string
+	addr       string
+	mongodbUrl string
+)
+
+// Global variables (database connection pools, etc)
+var (
+	err     error
+	mongodb *mgo.Session
 )
 
 // Init func
 func init() {
 	flag.StringVar(&addr, "addr", DefaultAddr, "Server host and port")
+	flag.StringVar(&mongodbUrl, "mongodbUrl", DefaultMongodbUrl, "Mongodb connection url")
 }
 
 // Main func
 func main() {
 	// Parse CLI arguments
 	flag.Parse()
+
+	// Connect to mongodb
+	mongodb, err = mgo.Dial(mongodbUrl)
+	if err != nil {
+		log.Fatalf("mgo.Dial: %v", err)
+		return
+	}
 
 	// Listen and serve
 	log.Print(fasthttp.ListenAndServe(addr, ApplyMiddleware(NewRouter().Handler)))
@@ -45,6 +62,7 @@ func NewRouter() *fasthttprouter.Router {
 func ApplyMiddleware(h fasthttp.RequestHandler) fasthttp.RequestHandler {
 
 	h = MiddlewareLogging(h)
+	h = MiddlewareMongodb(h)
 
 	return h
 }
--- middleware.go
+++ middleware.go
@@ -21,3 +21,21 @@ func MiddlewareLogging(h fasthttp.RequestHandler) fasthttp.RequestHandler {
 		log.Printf("%v %v - %v", method, request, status)
 	})
 }
+
+// Database middleware
+func MiddlewareMongodb(h fasthttp.RequestHandler) fasthttp.RequestHandler {
+	return fasthttp.RequestHandler(func(ctx *fasthttp.RequestCtx) {
+		// Copy session
+		session := mongodb.Copy()
+
+		// Get default database
+		db := session.DB("")
+
+		// Save in context
+		ctx.SetUserValue("mongodb", session)
+		ctx.SetUserValue("mongodb.db", db)
+
+		// Run handler
+		h(ctx)
+	})
+}
