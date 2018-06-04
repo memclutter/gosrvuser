e52732afc75fb2fd24693026e3ea0304a8d777a8
Memory Clutter <memclutter@gmail.com>
2018-06-04 12:26:36 +0300

    Amqp middleware

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"flag"
 	"log"
 
+	"time"
+
 	"github.com/buaazp/fasthttprouter"
 	"github.com/globalsign/mgo"
+	"github.com/streadway/amqp"
 	"github.com/valyala/fasthttp"
 )
 
@@ -13,24 +16,31 @@ import (
 const (
 	DefaultAddr       = ":8000"
 	DefaultMongodbUrl = "mongodb://localhost:27017/user"
+	DefaultAmqpUrl    = "amqp://guest:guest@localhost:5672/"
 )
 
 // CLI arguments
 var (
 	addr       string
 	mongodbUrl string
+	amqpUrl    string
 )
 
 // Global variables (database connection pools, etc)
 var (
-	err     error
-	mongodb *mgo.Session
+	err          error
+	mongodb      *mgo.Session
+	mq           *amqp.Connection
+	mqCloseError chan *amqp.Error
 )
 
 // Init func
 func init() {
 	flag.StringVar(&addr, "addr", DefaultAddr, "Server host and port")
 	flag.StringVar(&mongodbUrl, "mongodbUrl", DefaultMongodbUrl, "Mongodb connection url")
+	flag.StringVar(&amqpUrl, "amqpUrl", DefaultAmqpUrl, "AMQP connection url")
+
+	mqCloseError = make(chan *amqp.Error)
 }
 
 // Main func
@@ -39,12 +49,46 @@ func main() {
 	flag.Parse()
 
 	// Connect to mongodb
-	mongodb, err = mgo.Dial(mongodbUrl)
-	if err != nil {
-		log.Fatalf("mgo.Dial: %v", err)
-		return
+	for {
+		mongodb, err = mgo.Dial(mongodbUrl)
+		if err == nil {
+			break
+		}
+
+		log.Println(err)
+		log.Printf("Trying to reconnect to Mongodb at %s\n", mongodbUrl)
+		time.Sleep(500 * time.Millisecond)
 	}
 	defer mongodb.Close()
+
+	// Connect to amqp broker
+	go func() {
+		mqErr := new(amqp.Error)
+
+		for {
+			mqErr = <-mqCloseError
+			if mqErr != nil {
+				log.Printf("Connecting to %s\n", amqpUrl)
+
+				for {
+					mq, err = amqp.Dial(amqpUrl)
+
+					if err == nil {
+						break
+					}
+
+					log.Println(err)
+					log.Printf("Trying to reconnect to AMQP at %s\n", amqpUrl)
+					time.Sleep(500 * time.Millisecond)
+				}
+
+				mqCloseError = make(chan *amqp.Error)
+				mq.NotifyClose(mqCloseError)
+			}
+		}
+	}()
+	mqCloseError <- amqp.ErrClosed
+	defer mq.Close()
 
 	// Listen and serve
 	log.Print(fasthttp.ListenAndServe(addr, ApplyMiddleware(NewRouter().Handler)))
@@ -64,6 +108,7 @@ func ApplyMiddleware(h fasthttp.RequestHandler) fasthttp.RequestHandler {
 
 	h = MiddlewareLogging(h)
 	h = MiddlewareMongodb(h)
+	h = MiddlewareAmqp(h)
 
 	return h
 }
--- middleware.go
+++ middleware.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"strings"
 
+	"time"
+
+	"github.com/streadway/amqp"
 	"github.com/valyala/fasthttp"
 )
 
@@ -34,6 +37,30 @@ func MiddlewareMongodb(h fasthttp.RequestHandler) fasthttp.RequestHandler {
 		// Save in context
 		ctx.SetUserValue("mongodb", session)
 		ctx.SetUserValue("mongodb.db", db)
+
+		// Run handler
+		h(ctx)
+	})
+}
+
+// AMQP middleware
+func MiddlewareAmqp(h fasthttp.RequestHandler) fasthttp.RequestHandler {
+	return fasthttp.RequestHandler(func(ctx *fasthttp.RequestCtx) {
+		// Select amqp channel
+		var ch *amqp.Channel
+		for {
+			var err error
+
+			if ch, err = mq.Channel(); err == nil {
+				break
+			}
+
+			log.Println("Trying to reselect amqp channel")
+			time.Sleep(500 * time.Millisecond)
+		}
+
+		// Save in context
+		ctx.SetUserValue("amqp.ch", ch)
 
 		// Run handler
 		h(ctx)
